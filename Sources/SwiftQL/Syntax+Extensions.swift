593ab31e7958fd5124a53b460571f026fdf34d4b
Ian Keen <iankeen82@gmail.com>
2019-08-01 21:05:55 -0700

    Fix logic error

--- Sources/SwiftQL/Syntax+Extensions.swift
+++ Sources/SwiftQL/Syntax+Extensions.swift
@@ -3,10 +3,10 @@ import SwiftSyntax
 extension Syntax {
     public func firstSearchingUp<T: Syntax>(of: T.Type) -> T? {
         if let result = self as? T { return result }
-        return children.compactMap({ $0.firstSearchingUp(of: T.self) }).first
+        return parent?.firstSearchingUp(of: T.self)
     }
     public func firstSearchingDown<T: Syntax>(of: T.Type) -> T? {
         if let result = self as? T { return result }
-        return parent?.firstSearchingDown(of: T.self)
+        return children.compactMap({ $0.firstSearchingDown(of: T.self) }).first
     }
 }
